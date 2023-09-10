9000eaeef1e3ddfa16fcec6803bb064f8fae8da4
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-09-10 11:56:46 +0300

    Add files via upload

---
+++ Homework/Homework/Homework.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+//1) Fibonacci
+int Fibonacci(int a) // 1 1 2 3 5 8 13 21...
+{
+	if (a <= 2)
+		return 1;
+	else
+	{
+		int sum = 0;
+		int f1 = 1;
+		int f2 = 1;
+		for (int i = 2; i < a; i++)
+		{
+			sum = f1 + f2;
+			f1 = f2;
+			f2 = sum;
+		}
+		return sum;
+	}
+}
+int main()
+{
+	int a;
+	std::cin >> a;
+    std::cout << Fibonacci(a);
+}
+
