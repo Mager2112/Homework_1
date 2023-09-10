b28335bddadc1f9b7ceff36a637425e86ed458e2
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-09-10 14:16:41 +0300

    Update Homework.cpp

--- Homework/Homework/Homework.cpp
+++ Homework/Homework/Homework.cpp
@@ -19,10 +19,29 @@ int Fibonacci(int a) // 1 1 2 3 5 8 13 21...
 		return sum;
 	}
 }
-int main()
+//2.2) Array int
+int Array(int array[], int n)
 {
+	int result = 0;
+	std::sort(array, array + n);
+	for (int i = 0; i < n; i++)
+		std::cout << array[i] << " ";
+	for (int i = 0; i < n; i++)
+		if (array[i] != array[i + 1])
+			result++;
+	return result;
+}
+int main()
+{       //----------1--------
 	int a;
 	std::cin >> a;
 	std::cout << Fibonacci(a);
+	//----------2--------
+	int n;
+	std::cin >> n;
+	int *dinamich_array = new int[n];
+	for (int i = 0; i < n; i++)
+		std::cin >> dinamich_array[i];
+	std::cout <<"\n result: " << Array(dinamich_array, n);
 }
 
